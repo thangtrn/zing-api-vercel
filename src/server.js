2f263aad90b6991b64aac6e23a071eab47e94037
Thang Tran <thangtrn01@gmail.com>
2023-01-21 00:24:25 +0700

    deploy to vervel

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const Routes = require('./src/routes');
+const Routes = require('./routes');
 
 dotenv.config();
 const app = express();
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
-// app.use(express.static(path.join(__dirname, 'views')))
 
 // init Routes
 Routes(app);
