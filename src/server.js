3fe693bf5e94cd4fb32e30785bf8541ebb3b7977
Thang Tran <thangtrn01@gmail.com>
2023-01-29 21:59:40 +0700

    add cors origin

--- src/server.js
+++ src/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        '*',
+        'http://localhost:3000'
+    ],
+}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 
