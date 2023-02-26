1f76a8d6dda3659d07ff54c0ea89567ec7babf4c
Thang Tran <thangtrn01@gmail.com>
2023-02-26 16:01:29 +0700

    cors origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         '*',
-        'http://localhost:3000'
+        'http://localhost:3000',
+        'https://zing-music.vercel.app'
     ],
 }));
 app.use(express.json({limit: '50mb'}));
